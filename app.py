07c9370cfb0766971d2b7563ff096f0b4ad08a7a
Mahantesh Hiremath <mahanteshimath@gmail.com>
2024-10-06 14:15:22 +0000

    mmm

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ data = {
                      [5.6, 15.1, 20.2, 8.1],
                      [26.6, 22.8, 21.8, 24.0],
                      [22.3, 15.5, 13.4, 19.6]],
-    "Countries": ["United Kingdom United Kingdom United Kingdom", "United States United States", "Brazil", "Germany"],
+    "Countries": ["United Kingdom United Kingdom United Kingdom United Kingdom United Kingdom United Kingdom", "United States United States", "Brazil", "Germany"],
     "Seasons": ["Winter", "Spring", "Summer", "Autumn"]
 }
 
@@ -15,7 +15,7 @@ data = {
 max_length = max(len(country) for country in data["Countries"])
 
 dynamic_margin =  (max_length * 10)  # Adjust margin based on label length
-dynamic_margin = max_length
+
 layout = {"margin": {"l": dynamic_margin}}
 
 md = "<|{data}|chart|type=heatmap|z=Temperatures|x=Seasons|y=Countries|layout={layout}|>"
